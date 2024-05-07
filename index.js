a675d1704493900fcbdb8198ea604801ef785867
hoangcongminh11 <cassie8228nam1311@outlook.com>
2024-05-07 22:39:59 +0700

    update aptos project commit

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-function isSubsequence(s, t) {
-  let i = 0;
-  let j = 0;
-  while (i < s.length && j < t.length) {
-    if (s[i] === t[j]) i++;
-    j++;
-  }
-  return i === s.length;
+function isPalindrome(str) {
+  const reversed = str.split("").reverse().join("");
+  return str === reversed;
 }
+
+console.log(isPalindrome("racecar")); // true
