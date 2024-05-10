70ba12991dbbcb9f235e38cd2203f3dc4e5ed4ad
hoangcongminh11 <cassie8228nam1311@outlook.com>
2024-05-11 01:30:09 +0700

    init project

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function rotateArray(arr, n) {
-  return arr.slice(n).concat(arr.slice(0, n));
+function isValidBST(root) {
+  let prev = null;
+  return inorder(root);
+  function inorder(node) {
+    if (!node) return true;
+    if (!inorder(node.left)) return false;
+    if (prev !== null && node.val <= prev) return false;
+    prev = node.val;
+    return inorder(node.right);
+  }
 }
